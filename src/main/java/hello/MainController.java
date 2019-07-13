5991ef7eb13689bae65893c635e892b8db2c673d
Victor Lopez <lvictor_9@hotmail.com>
2019-07-13 15:35:52 -0600

    First-based first-served CRUD

--- src/main/java/hello/MainController.java
+++ src/main/java/hello/MainController.java
@@ -3,6 +3,7 @@ package hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -11,5 +12,17 @@ public class MainController {
     public String index(){
         return "index";
     }
+
+    @GetMapping("/crud")
+    public String crud(Model model, @RequestParam(defaultValue="Users") String kind) {
+
+        model.addAttribute("kind", kind);
+
+        return "crud";
+
+    }
+
+    @GetMapping("/resources")
+    public String resources() { return "resources"; }
 
 }
